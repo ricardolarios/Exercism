732899c38e3a42ba77b960b6d97ff748b99d44c5
ricardolarios <ricardolarios98@gmail.com>
2018-09-05 11:48:43 -0400

    Add simple-cipher and nucleotide-count exercises.

---
+++ javascript/simple-cipher/simple-cipher.js
@@ -0,0 +1,15 @@
+class Cipher {
+	constructor(key) {
+		this.key = key;
+	}
+
+	// Encodes the given string according to this cipher's key.
+	encode(given) {
+
+	}
+
+	// Decodes the given string according to this cipher's key.
+	decode(given) {
+
+	}
+}
