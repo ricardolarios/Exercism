48ecb110e1026aa4e8adef48ce2d35405accdedb
ricardolarios <ricardolarios98@gmail.com>
2018-10-04 12:11:20 -0400

    Started Bob, finished Series.

---
+++ javascript/series/series.js
@@ -0,0 +1,25 @@
+class Series {
+	// Constructs a new series with an array of the given digits.
+	constructor(digits) {
+		this.digits = digits.split('').map(x => parseInt(x));
+		this.length = this.digits.length;
+	}
+
+	slices(len) {
+		if (len > this.length) {
+			throw new Error('Slice size is too big.');
+		}
+		else if (len <= this.length) {
+			let i = 0;
+			let result = [];
+			while (i + len <= this.length) {
+				result.push(this.digits.slice(i, i + len))
+				i += 1;
+			}
+
+			return result;
+		}
+	}
+}
+
+export default Series;
