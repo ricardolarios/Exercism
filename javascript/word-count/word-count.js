23e361c2659720e27e28d26a85be27397fd3440d
ricardolarios <ricardolarios98@gmail.com>
2018-11-02 09:31:28 -0400

    Update merge dependency

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,9 +1,22 @@
 export default class Words {
 	count(given) {
-		const addToMap = function() => {
-			
+		/* This is how it fucking should be instead of using objects, assholes.
+		let result = new Map();
+		const addToMap = function(cur) {
+			result.has(cur) ? result.set(cur, result.get(cur) + 1) : result.set(cur, 1);
 		}
-		return given.toLowerCase().split(/[\s|\n|]+/).forEach();
+	
+		given.toLowerCase().split(/[\s|\n|]+/).forEach(addToMap);
+		return result;
+		*/
+
+		let result = {};
+		const addToMap = function(cur) {
+			result.has(cur) ? result.set(cur, result.get(cur) + 1) : result.set(cur, 1);
+		}
+	
+		given.toLowerCase().split(/[\s|\n|]+/).forEach(addToMap);
+		return result;
 	}
 }
 
